6630cf4b6df7ef023acff042c058fcd2d3f4f1b2
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2022-07-11 18:56:06 -0300

    feat: adding the font awesemo lib

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,3 +1,4 @@
+import '@fortawesome/fontawesome-free/css/all.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'vuetify/dist/vuetify.min.css'
@@ -5,4 +6,7 @@ import 'vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'fa',
+      },
 });
